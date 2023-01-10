abe80f8c9029e5d302cc709574c3e652fe7a4c99
Henit Chobisa <chobisa.henit@gmail.com>
2023-01-10 10:03:04 +0000

    feat: Docker-Compose Auto Download Initialized

--- Packages/DockerCompose/main.go
+++ Packages/DockerCompose/main.go
@@ -9,14 +9,22 @@ import (
 	"time"
 )
 
-// func downloadAppDockerCompose() error {
-// 	spinner := Figure.Spinner("🚀 Downloading preconfigured docker-compose.yml from web...", Colors.Blue(), "")
-// 	spinner.Start()
-// 	cmd := exec.Command("wget", "")
-// }
+func downloadAppDockerCompose() error {
+	spinner := Figure.Spinner("🚀 Downloading preconfigured docker-compose.yml from web...", Colors.Blue(), "")
+	spinner.Start()
+	cmd := exec.Command("wget", "https://raw.githubusercontent.com/henit-chobisa/RC-Test-Environment-Companion/main/Accessories/docker-compose.yml")
+
+	_, err := cmd.CombinedOutput()
+	spinner.Stop()
+
+	if err != nil {
+		return err
+	}
+	fmt.Println(Colors.Yellow() + "Successfully downloaded docker-compose.yml from source")
+	return nil
+}
 
 func Up(path string) error {
-
 	spinner := Figure.Spinner("🚀 Starting Rocket Chat Server using Docker Compose file.", Colors.Yellow(), "")
 	spinner.Start()
 	time.Sleep(2 * time.Second)
@@ -27,11 +35,14 @@ func Up(path string) error {
 	if err != nil {
 		spinner.Stop()
 		fmt.Printf(Colors.Red()+"Docker-Compose Error : %v", err)
+		if err.Error() == "exit status 14" {
+			downloadAppDockerCompose()
+			Up("./docker-compose.yml")
+		}
 		return err
 	}
 	spinner.Stop()
 	fmt.Printf("\n" + Colors.Yellow() + "🚀 Started Rocket Chat Server using Docker Compose file.\n")
 	fmt.Println(Colors.Yellow(), fmt.Sprint("\n\n", bytes.NewBuffer(p)))
-
 	return nil
 }
