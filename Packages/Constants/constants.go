bcce97744fc98718362be8e5ddd632083e515acf
Henit Chobisa <chobisa.henit@gmail.com>
2023-01-14 12:02:08 +0000

    fix: Colors and line added as constants

--- Packages/Constants/constants.go
+++ Packages/Constants/constants.go
@@ -1,8 +1,22 @@
 package constants
 
+// configuration parameters
 const (
 	AppDir_mode_deep      = "appDir_deep"
 	AppDir_mode_shallow   = "appDir_shallow"
 	DockerCompose_default = "docker-compose.yml"
 	AppDir_default        = "./"
 )
+
+// Colors
+const (
+	red    = "\033[31m"
+	green  = "\033[32m"
+	yellow = "\033[33m"
+	blue   = "\033[34m"
+	purple = "\033[35m"
+	cyan   = "\033[36m"
+	white  = "\033[37m"
+)
+
+const line = "-----------------------------------------------------------------------------------------------------------------------------------------"
