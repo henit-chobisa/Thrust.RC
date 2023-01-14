dd3d7222596920a456a512a74bf21d8b343492cd
Henit Chobisa <chobisa.henit@gmail.com>
2023-01-14 17:19:14 +0000

    feat: Stop Command Initilized for Cobra

---
+++ cmd/stop.go
@@ -0,0 +1,30 @@
+package cmd
+
+import (
+	constants "RCTestSetup/Packages/Constants"
+
+	"github.com/spf13/cobra"
+)
+
+var rma bool
+var rmv bool
+var rmc bool
+
+var stopCommand = &cobra.Command{
+	Use:                   "stop [Flags] <directory>",
+	Short:                 "This stops all the containers responsible for running the environment",
+	Long:                  "This command stops and optionally removes containers and volumes such as Rocket.Chat mongodb etc responsible for execution of the companion, if removed the companion has to download them again.",
+	DisableFlagsInUseLine: true,
+	Args:                  cobra.ExactArgs(1),
+}
+
+func init() {
+	rootCmd.AddCommand(stopCommand)
+	initStopFlags()
+}
+
+func initStopFlags() {
+	stopCommand.Flags().BoolVarP(&rmv, "remove-images", "v", false, constants.Blue+"This removes all volumes that were generated while starting the companion"+constants.White)
+	stopCommand.Flags().BoolVarP(&rmc, "remove-containers", "c", true, constants.Blue+"This removes all the images that were generated while starting the companion"+constants.White)
+	stopCommand.Flags().BoolVarP(&rma, "remove-all", "a", false, constants.Blue+"This removes both the images and volumes that were generated while starting the companion"+constants.White)
+}
