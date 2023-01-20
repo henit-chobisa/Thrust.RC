22fc5d9c2cb4a9f0dad9b5888df1be63ea8f6a42
Henit Chobisa <chobisa.henit@gmail.com>
2023-01-20 21:01:20 +0000

    feat: Added Default Rocket.Chat Container

---
+++ Packages/DockerSDK/DefaultContainers/RocketChat.go
@@ -0,0 +1,33 @@
+package DefaultContainers
+
+import (
+	constants "RCTestSetup/Packages/Constants"
+	"RCTestSetup/Packages/DockerSDK"
+
+	"github.com/docker/go-connections/nat"
+)
+
+func LaunchRocketChatContainer(sdk DockerSDK.Docker, networkID string) (string, error) {
+	containerID, err := sdk.CreateContainer(networkID, "rocketchat", constants.RocketChatImage, nat.PortSet{
+		"3000/tcp": {},
+	}, nat.PortMap{
+		"3000/tcp": []nat.PortBinding{
+			{
+				HostIP:   "0.0.0.0",
+				HostPort: "3000",
+			},
+		},
+	}, []string{
+		"MONGO_URL=mongodb://mongo:27017/rocketchat?replicaSet=rs0",
+		"MONGO_OPLOG_URL=mongodb://mongo:27017/local?replicaSet=rs0",
+		"ROOT_URL=http://localhost:3000",
+		"PORT=3000",
+		"DEPLOY_METHOD=docker",
+		"OVERWRITE_SETTING_Apps_Framework_Development_Mode=true",
+		"OVERWRITE_SETTING_Show_Setup_Wizard=Completed",
+	}, nil, nil, []string{
+		"rocketchat",
+	}, nil, nil)
+
+	return containerID, err
+}
