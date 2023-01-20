b2e0e1014e25c71887d35087f5b4ade0dc265711
Henit Chobisa <chobisa.henit@gmail.com>
2023-01-20 21:02:08 +0000

    feat: Loaded Handler with Starting Default Network Methods

--- Packages/Handlers/Handler.go
+++ Packages/Handlers/Handler.go
@@ -3,17 +3,20 @@ package Handlers
 import (
 	constants "RCTestSetup/Packages/Constants"
 	"RCTestSetup/Packages/DockerSDK"
+	"RCTestSetup/Packages/DockerSDK/DefaultContainers"
+	initiateadmin "RCTestSetup/Packages/InitiateAdmin"
 	"RCTestSetup/Utils"
 	"RCTestSetup/tui/components/Page"
 	"fmt"
 	"sync"
+	"time"
 
 	"github.com/docker/docker/api/types/filters"
 )
 
 func HandleDependencyCheck() error {
 	// confirming initial configuration dependency
-	fmt.Printf(constants.Blue + "\nChecking Initial dependencies required for running the companion\n\n")
+	fmt.Printf(constants.Blue + "\n🐳 Checking Initial dependencies required for running the companion\n\n")
 	dependencyModel := Page.NewDependencyModel()
 	fmt.Println(dependencyModel.View())
 
@@ -26,7 +29,7 @@ func HandleDependencyCheck() error {
 func HandlePullingImages() (map[string]string, error) {
 	// first let's check for the images that are currently present in the system
 
-	fmt.Printf(constants.Blue + "Verifying Required Docker Images for Companion\n\n" + constants.White)
+	fmt.Printf(constants.Blue + "🐳 Verifying Required Docker Images for Companion\n\n" + constants.White)
 
 	client, err := DockerSDK.GetNewClient()
 	if err != nil {
@@ -64,7 +67,7 @@ func HandlePullingImages() (map[string]string, error) {
 }
 
 func PullImages(images map[string]string) error {
-	fmt.Printf(constants.Blue + "\nPulling Required Images\n" + constants.White)
+	fmt.Printf(constants.Blue + "\n🐳 Pulling Required Images\n" + constants.White)
 
 	var tasks sync.WaitGroup
 	tasks.Add(len(images))
@@ -83,6 +86,110 @@ func PullImages(images map[string]string) error {
 	tasks.Wait()
 
 	fmt.Println()
+
+	client.Client.Close()
+
+	return nil
+}
+
+func CheckRequiredContainers() (containersToStart map[string]string, companionStart bool, err error) {
+	fmt.Printf(constants.Blue + "\n🐳 Finding Running Containers\n\n" + constants.White)
+	client, err := DockerSDK.GetNewClient()
+	if err != nil {
+		return nil, false, err
+	}
+
+	filter := filters.NewArgs()
+
+	filter.Add("ancestor", constants.RocketChatImage)
+	filter.Add("ancestor", constants.MongoDBImage)
+	filter.Add("ancestor", constants.CompanionImage)
+	filter.Add("status", "running")
+	filter.Add("network", "RCAPPSDEFAULT")
+
+	containers, err := client.FindContainers(filter)
+
+	if err != nil {
+		return nil, false, err
+	}
+
+	containersToStart = map[string]string{constants.RocketChatImage: "Rocket.Chat", constants.MongoDBImage: "MongoDB", constants.CompanionImage: "Apps.Companion"}
+
+	companionStart = false
+	for _, container := range *containers {
+		fmt.Println(Utils.Tick() + containersToStart[container.Image] + " : " + container.ID + " : " + container.Names[0] + " : " + container.Image + " : " + container.Status)
+		delete(containersToStart, container.Image)
+	}
+
+	for key, value := range containersToStart {
+		if value == "Apps.Companion" {
+			companionStart = true
+		}
+		fmt.Println(Utils.Cross() + value + " : " + key)
+	}
+
+	return containersToStart, companionStart, nil
+}
+
+func createDefaultNetwork() (id string, err error) {
+
+	client, err := DockerSDK.GetNewClient()
+
+	if err != nil {
+		return "", nil
+	}
+
+	id, err = client.CreateNetwork()
+	return id, err
+}
+
+func StartContainersWithDefaultNetwork(containers map[string]string) error {
+
+	fmt.Printf(constants.Blue + "\n🐳 Starting Required Containers for Companion\n\n" + constants.White)
+
+	defaultNetworkId, err := createDefaultNetwork()
+
+	if err != nil {
+		return err
+	}
+
+	client, err := DockerSDK.GetNewClient()
+
+	if err != nil {
+		return err
+	}
+	if _, ok := containers[constants.MongoDBImage]; ok {
+		mongodbID, err := DefaultContainers.LaunchMongoDbContainer(*client, defaultNetworkId)
+
+		if err != nil {
+			fmt.Println(Utils.Cross() + "Error Starting Mongodb Container for Rocket.Chat, Aborting Operation")
+			return err
+		}
+
+		fmt.Println(Utils.Tick() + "Started Mongodb Container for Rocket.Chat with ID " + mongodbID)
+	}
+
+	if _, ok := containers[constants.RocketChatImage]; ok {
+		rocketChatID, err := DefaultContainers.LaunchRocketChatContainer(*client, defaultNetworkId)
+
+		if err != nil {
+			fmt.Println(Utils.Cross() + "Error Starting Rocket.Chat Container for Rocket.Chat, Aborting Operation")
+			return err
+		}
+
+		fmt.Println(Utils.Tick() + "Started Rocket.Chat Container with ID " + rocketChatID)
+	}
+
+	time.Sleep(10 * time.Second)
+
+	return nil
+}
+
+func CreateAdminUser() error {
+
+	fmt.Printf(constants.Blue + "\n🐳 Creating Admin User for Rocket.Chat Instance\n\n" + constants.White)
+
+	initiateadmin.Initiate()
 
 	return nil
 }
