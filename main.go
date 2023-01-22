9d4de95233dce3093643d1930e1254b001ed156a
Henit Chobisa <chobisa.henit@gmail.com>
2023-01-22 20:28:03 +0000

    chore: env and main

--- main.go
+++ main.go
@@ -6,14 +6,4 @@ import (
 
 func main() {
 	cmd.Execute()
-
-	// InitiatePhase1(data, appDir)
-	// InitiatePhase2(data)
-	// InitiatePhase3(data, appDir)
-
-	// exec.Command("gp", "preview", "http://localhost:3000", "--external").Output()
-
-	// if data["watcher"] == nil || data["watcher"] == true || data["watcher"] == "true" {
-	// 	watcher.Watch(appDir, fmt.Sprintf("%v", data["watcherMode"]))
-	// }
 }
