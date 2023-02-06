e2217f2240c48acf2fafc8b637e9b6443276bb91
Henit Chobisa <chobisa.henit@gmail.com>
2023-02-06 09:59:32 +0000

    feat: Modified Companion Container with Container Model

--- Packages/DockerSDK/DefaultContainers/Companion.go
+++ Packages/DockerSDK/DefaultContainers/Companion.go
@@ -4,17 +4,35 @@ import (
 	"fmt"
 	constants "thrust/Packages/Constants"
 	"thrust/Packages/DockerSDK"
+	models "thrust/Packages/Models"
 	"thrust/Utils"
 )
 
 func LaunchCompanionContainer(sdk DockerSDK.Docker, networkID string, path string, name string) (containerID string, err error) {
-	containerID, err = sdk.CreateContainer(networkID, name+Utils.RandomString(5), constants.CompanionImage, nil, nil, []string{
-		"url=http://rocketchat:3000",
-		"username=user0",
-		"password=123456",
-	}, nil, []string{
-		fmt.Sprintf("%v:/app", path),
-	}, []string{"Companion"}, nil, nil, true, nil, true)
+
+	companionContainer := models.Container{
+		NetworkID:     networkID,
+		ContainerName: name + Utils.RandomString(5),
+		Image:         constants.CompanionImage,
+		ExposedPort:   nil,
+		PortBindings:  nil,
+		Env: []string{
+			"url=http://rocketchat:3000",
+			"username=user0",
+			"password=123456",
+		},
+		Volumes: nil,
+		Binds: []string{
+			fmt.Sprintf("%v:/app", path),
+		},
+		Aliases:  []string{"Companion"},
+		Links:    nil,
+		Mount:    nil,
+		Commands: nil,
+		Stdout:   true,
+	}
+
+	containerID, err = sdk.CreateContainer(companionContainer, true)
 
 	return
 }
