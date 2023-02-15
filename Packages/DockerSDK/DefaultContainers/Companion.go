c775a9ade671daf8b4dd924d3a049964c1dd85c3
Henit Chobisa <chobisa.henit@gmail.com>
2023-02-15 07:00:10 +0000

    feat: Removed Ramdomizer from Companion Container

--- Packages/DockerSDK/DefaultContainers/Companion.go
+++ Packages/DockerSDK/DefaultContainers/Companion.go
@@ -5,14 +5,13 @@ import (
 	constants "thrust/Packages/Constants"
 	"thrust/Packages/DockerSDK"
 	models "thrust/Packages/Models"
-	"thrust/Utils"
 )
 
 func LaunchCompanionContainer(sdk DockerSDK.Docker, networkID string, path string, name string) (containerID string, err error) {
 
 	companionContainer := models.Container{
 		NetworkID:     networkID,
-		ContainerName: name + Utils.RandomString(5),
+		ContainerName: name,
 		Image:         constants.CompanionImage,
 		ExposedPort:   nil,
 		PortBindings:  nil,
@@ -25,11 +24,12 @@ func LaunchCompanionContainer(sdk DockerSDK.Docker, networkID string, path strin
 		Binds: []string{
 			fmt.Sprintf("%v:/app", path),
 		},
-		Aliases:  []string{"Companion"},
-		Links:    nil,
-		Mount:    nil,
-		Commands: nil,
-		Stdout:   true,
+		Aliases:    []string{"Companion"},
+		Links:      nil,
+		Mount:      nil,
+		Commands:   nil,
+		Stdout:     true,
+		AutoRemove: true,
 	}
 
 	containerID, err = sdk.CreateContainer(companionContainer, true)
