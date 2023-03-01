251d9f51943c3136c9a94eafd59f069bf2224c4a
Henit Chobisa <chobisa.henit@gmail.com>
2023-03-01 04:55:16 +0000

    fix: Updated Handler and Intiate Admin for Viper Config

--- Packages/Handlers/Handler.go
+++ Packages/Handlers/Handler.go
@@ -17,6 +17,7 @@ import (
 
 	"github.com/docker/docker/api/types"
 	"github.com/docker/docker/api/types/filters"
+	"github.com/spf13/viper"
 )
 
 func HandleDependencyCheck() error {
@@ -34,6 +35,8 @@ func HandleDependencyCheck() error {
 func HandlePullingImages() (map[string]string, error) {
 	// first let's check for the images that are currently present in the system
 
+	isAppMode := viper.GetBool("isAppMode")
+
 	fmt.Printf(constants.Blue + "🐳 Verifying Required Docker Images for Companion\n\n" + constants.White)
 
 	client, err := DockerSDK.GetNewClient()
@@ -46,7 +49,10 @@ func HandlePullingImages() (map[string]string, error) {
 
 	filter.Add("reference", constants.RocketChatImage)
 	filter.Add("reference", constants.MongoDBImage)
-	filter.Add("reference", constants.CompanionImage)
+
+	if isAppMode {
+		filter.Add("reference", constants.CompanionImage)
+	}
 
 	imageSummary, err := client.FindImages(filter)
 
@@ -55,7 +61,11 @@ func HandlePullingImages() (map[string]string, error) {
 		return nil, err
 	}
 
-	imagesToPull := map[string]string{constants.RocketChatImage: "Rocket.Chat", constants.MongoDBImage: "MongoDB", constants.CompanionImage: "Apps.Companion"}
+	imagesToPull := map[string]string{constants.RocketChatImage: "Rocket.Chat", constants.MongoDBImage: "MongoDB"}
+
+	if isAppMode {
+		imagesToPull[constants.CompanionImage] = "Apps.Companion"
+	}
 
 	for _, image := range *imageSummary {
 		fmt.Println(Utils.Tick() + imagesToPull[image.RepoTags[0]] + " : " + image.RepoTags[0])
@@ -102,46 +112,59 @@ func getRunningContainers(client *DockerSDK.Docker) (*[]types.Container, error) 
 
 	filter.Add("ancestor", constants.RocketChatImage)
 	filter.Add("ancestor", constants.MongoDBImage)
-	filter.Add("ancestor", constants.CompanionImage)
+	if viper.GetBool("appMode") {
+		filter.Add("ancestor", constants.CompanionImage)
+	}
 	filter.Add("status", "running")
 	filter.Add("network", "RCAPPSDEFAULT")
 
 	return client.FindContainers(filter)
 }
 
-func CheckRequiredContainers(app *models.AppInfo) (containersToStart map[string]string, companionStart bool, companionID string, err error) {
+func CheckRequiredContainers(app *models.AppInfo) (containersToStart map[string]string, companionStart bool, containerIDs map[string]string, err error) {
 	fmt.Printf(constants.Blue + "\n🐳 Finding Running Containers\n\n" + constants.White)
 	client, err := DockerSDK.GetNewClient()
+	containerIDs = map[string]string{}
+	isAppMode := viper.GetBool("appMode")
+
 	if err != nil {
-		return nil, false, "", err
+		return nil, false, containerIDs, err
 	}
 
 	if err != nil {
-		return nil, false, "", err
+		return nil, false, containerIDs, err
 	}
 
 	containers, err := getRunningContainers(client)
 
 	if err != nil {
-		return nil, false, "", err
+		return nil, false, containerIDs, err
 	}
 
-	containersToStart = map[string]string{constants.RocketChatImage: "Rocket.Chat", constants.MongoDBImage: "MongoDB", constants.CompanionImage: "Apps.Companion"}
+	containersToStart = map[string]string{constants.RocketChatImage: "Rocket.Chat", constants.MongoDBImage: "MongoDB"}
 
-	companionName := "/companion_" + app.Name + app.Id + app.Version
+	companionName := ""
+
+	if isAppMode {
+		containersToStart[constants.CompanionImage] = "Apps.Companion"
+		companionName = "/companion_" + app.Name + app.Id + app.Version
+	}
 
 	companionStart = false
 
 	for _, container := range *containers {
 		if companionName != container.Names[0] && !strings.HasPrefix(container.Names[0], "/companion") {
 			fmt.Println(Utils.Tick() + containersToStart[container.Image] + " : " + container.ID + " : " + container.Names[0] + " : " + container.Image + " : " + container.Status)
+
+			containerIDs[container.Image] = container.ID
+
 			delete(containersToStart, container.Image)
 			continue
 		} else if companionName == container.Names[0] {
 			fmt.Println(Utils.Tick() + containersToStart[container.Image] + " : " + container.ID + " : " + container.Names[0] + " : " + container.Image + " : " + container.Status)
 
 			if container.Image == constants.CompanionImage {
-				companionID = container.ID
+				containerIDs[constants.CompanionImage] = container.ID
 			}
 
 			if len(containersToStart) > 0 {
@@ -157,7 +180,7 @@ func CheckRequiredContainers(app *models.AppInfo) (containersToStart map[string]
 		fmt.Println(Utils.Cross() + value + " : " + key)
 	}
 
-	return containersToStart, companionStart, companionID, nil
+	return containersToStart, companionStart, containerIDs, nil
 }
 
 func createDefaultNetwork() (id string, err error) {
@@ -172,44 +195,56 @@ func createDefaultNetwork() (id string, err error) {
 	return id, err
 }
 
-func StartContainersWithDefaultNetwork(containers map[string]string) error {
+func StartContainersWithDefaultNetwork(containers map[string]string) (containerIDs map[string]string, err error) {
 
 	fmt.Printf(constants.Blue + "\n🐳 Starting Required Containers for Companion\n\n" + constants.White)
+
+	containerIDs = map[string]string{constants.RocketChatImage: "Rocket.Chat", constants.MongoDBImage: "MongoDB"}
 
 	defaultNetworkId, err := createDefaultNetwork()
 
 	if err != nil {
-		return err
+		return containerIDs, err
 	}
 
 	client, err := DockerSDK.GetNewClient()
 
 	if err != nil {
-		return err
+		return containerIDs, err
 	}
 	if _, ok := containers[constants.MongoDBImage]; ok {
 		mongodbID, err := DefaultContainers.LaunchMongoDbContainer(*client, defaultNetworkId)
 
 		if err != nil {
 			fmt.Println(Utils.Cross() + "Error Starting Mongodb Container for Rocket.Chat, Aborting Operation")
-			return err
+			return containerIDs, err
 		}
+
+		containerIDs[constants.MongoDBImage] = mongodbID
 
 		fmt.Println(Utils.Tick() + "Started Mongodb Container for Rocket.Chat with ID " + mongodbID)
 	}
 
 	if _, ok := containers[constants.RocketChatImage]; ok {
+		if !viper.GetBool("appMode") {
+			fmt.Println(Utils.Tick() + "Starting Rocket.Chat Container for assitance")
+		}
 		rocketChatID, err := DefaultContainers.LaunchRocketChatContainer(*client, defaultNetworkId)
 
 		if err != nil {
 			fmt.Println(Utils.Cross() + "Error Starting Rocket.Chat Container for Rocket.Chat, Aborting Operation")
-			return err
+			return containerIDs, err
+		}
+		fmt.Println(rocketChatID + " Rocket.Chat ID")
+		containerIDs[constants.RocketChatImage] = rocketChatID
+
+		if viper.GetBool("appMode") {
+			fmt.Println(Utils.Tick() + "Started Rocket.Chat Container with ID " + rocketChatID)
 		}
 
-		fmt.Println(Utils.Tick() + "Started Rocket.Chat Container with ID " + rocketChatID)
 	}
 
-	return nil
+	return containerIDs, nil
 }
 
 func CreateAdminUser() error {
@@ -269,7 +304,11 @@ func Cleanup(app *models.AppInfo) error {
 
 	runningContainers, err := getRunningContainers(client)
 
-	companionName := "/companion_" + app.Name + app.Id + app.Version
+	var companionName string = ""
+
+	if viper.GetBool("appMode") {
+		companionName = "/companion_" + app.Name + app.Id + app.Version
+	}
 
 	if err != nil {
 		return err
@@ -279,7 +318,11 @@ func Cleanup(app *models.AppInfo) error {
 	wg.Add(len(*runningContainers))
 	fmt.Println(len(*runningContainers))
 	for _, container := range *runningContainers {
-		if len(*runningContainers) == 3 || container.Names[0] == companionName {
+		if viper.GetBool("appMode") {
+			if len(*runningContainers) == 3 || container.Names[0] == companionName {
+				go client.RemoveContainer(wg, container.ID)
+			}
+		} else {
 			go client.RemoveContainer(wg, container.ID)
 		}
 	}
